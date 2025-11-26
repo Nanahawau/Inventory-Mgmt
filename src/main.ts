ca09d8f305a9448532ac97d108e9f1d133b3619c
Nana Adeku <nanaadeku@gmail.com>
2025-11-26 00:47:08 +0000

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   app.enableCors({
+    origin: ['http://localhost:3001'], // frontend origin(s)
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
